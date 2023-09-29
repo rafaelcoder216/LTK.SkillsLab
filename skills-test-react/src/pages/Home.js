918fb6cf29cdb37a9398db7aa8c11a05aa956ed3
Rafael Lee <rafael.lee0216@gmail.com>
2023-09-28 20:33:28 -0400

    added: Router

---
+++ skills-test-react/src/pages/Home.js
@@ -0,0 +1,31 @@
+import * as React from "react";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import AccountMenu from "../AccountMenu";
+import BasicForm from "../BasicForm";
+
+const Home = () => {
+  const [openForm, setOpenForm] = React.useState(false);
+  const handleOnClick = (page) => {
+    console.log("open");
+    if (!openForm) {
+      setOpenForm(true);
+    } else {
+      setOpenForm(false);
+    }
+  };
+  return (
+    <Container maxWidth="sm">
+      <AccountMenu onClick={handleOnClick} />
+      {openForm && <BasicForm />}
+      <Box sx={{ my: 4 }}>
+        <Typography variant="h4" component="h1" gutterBottom>
+          React Skills test
+        </Typography>
+      </Box>
+    </Container>
+  );
+};
+
+export default Home;
