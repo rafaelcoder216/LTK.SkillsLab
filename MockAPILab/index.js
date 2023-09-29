f123b357433c610a6b738fdc61ed445f04a18a1a
Rafael Lee <rafael.lee0216@gmail.com>
2023-09-28 21:10:45 -0400

    added: loanRoutes

--- MockAPILab/index.js
+++ MockAPILab/index.js
@@ -1,27 +1,33 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+const loanRoutes = require("./routes/ltk");
 const port = 3000;
 const isLocal = true;
 
-app.get('/', (req, res) => {
-    res.json({
-		message: "✨ 👋🌏 ✨",
-		stage: process.env.NODE_ENV,
-	});
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "✨ 👋🌏 ✨",
+    stage: process.env.NODE_ENV,
+  });
 });
 
 app.get("/ping", (req, res) => {
-	res.json({
-		message: "🏓",
-	});
+  res.json({
+    message: "🏓",
+  });
 });
 
+app.use("/loans", loanRoutes);
+
 if (isLocal) {
-	//local host
-	app.listen(port, () => {
-		console.log(`Example app listening on port ${port}`)
-	});
+  //local host
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+  });
 } else {
-	//for lambda export
-	module.exports = app;
+  //for lambda export
+  module.exports = app;
 }
