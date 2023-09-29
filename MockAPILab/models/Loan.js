19119880f230fa5208cf88b9e53f19126dd99409
Rafael Lee <rafael.lee0216@gmail.com>
2023-09-28 21:36:17 -0400

    added: CRUD

---
+++ MockAPILab/models/Loan.js
@@ -0,0 +1,8 @@
+class Loan {
+  constructor(loanId, borrowers) {
+    this.loanId = loanId;
+    this.borrowers = borrowers;
+  }
+}
+
+module.exports = Loan;
--- MockAPILab/routes/ltk/index.js
+++ MockAPILab/routes/ltk/index.js
@@ -1,4 +1,86 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const Loan = require("../../models/Loan");
+
+const loans = [];
+let loanIdCounter = 1;
+
+router.get("/", (req, res) => {
+  res.json(loans);
+});
+
+router.post("/", (req, res) => {
+  const { borrowers } = req.body;
+  if (!borrowers || !Array.isArray(borrowers) || !borrowers.length) {
+    res.status(400).json({ error: "Invalid Data" });
+  } else {
+    const newLoan = new Loan(loanIdCounter++, borrowers);
+    loans.push(newLoan);
+    res.status(201).json(newLoan);
+  }
+});
+
+router.get("/:loanId", (req, res) => {
+  const loanId = parseInt(req.params.loanId);
+  const loan = loans.find((loan) => loan.loanId === loanId);
+  if (!loan) {
+    res.status(404).json({ error: "Loan not found" });
+  } else {
+    res.json(loan);
+  }
+});
+
+router.patch("/:loanId/borrowers/:pairId", (req, res) => {
+  const loanId = parseInt(req.params.loanId);
+  const pairId = parseInt(req.params.pairId);
+
+  const loan = loans.find((loan) => loan.loanId === loanId);
+  if (!loan) {
+    res.status(404).json({ error: "Loan not found" });
+  } else {
+    const borrower = loan.borrowers.find(
+      (borrower) => borrower.pairId === pairId
+    );
+    if (!borrower) {
+      res.status(404).json({ error: "Borrow not found" });
+    } else {
+      Object.assign(borrower, req.body);
+      res.json(borrower);
+    }
+  }
+});
+
+router.delete("/:loanId/borrowers/:pairId", (req, res) => {
+  const loanId = parseInt(req.params.loanId);
+  const pairId = parseInt(req.params.pairId);
+
+  const loan = loans.find((loan) => loan.loanId === loanId);
+  if (!loan) {
+    res.status(404).json({ error: "Loan not found" });
+  } else {
+    const borrowerIndex = loan.borrowers.findIndex(
+      (borrower) => borrower.pairId === pairId
+    );
+    if (borrowerIndex === -1) {
+      res.status(404).json({ error: "Borrow not found" });
+    } else {
+      const deleteBorrower = loan.borrowers.splice(borrowerIndex, 1);
+      res.json(deleteBorrower[0]);
+    }
+  }
+});
+
+router.delete("/:loandId", (req, res) => {
+  const loanId = parseInt(req.params.loandId);
+
+  const loanIndex = loans.find((loan) => loan.loanId === loanId);
+
+  if (loanIndex === -1) {
+    res.status(404).json({ error: "Loan not found" });
+  } else {
+    const deleteLoan = loans.splice(loanIndex, 1);
+    res.json(deleteLoan[0]);
+  }
+});
 
 module.exports = router;
